0684dbad98d1b0e4e14c2a3568ca2c99b81e14c2
unknown <laicheehoong@gmail.com>
2021-01-03 07:57:52 +0800

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React from 'react';
+
+import { CardList } from './components/card-list/card-list.component.jsx';
+
+import { SearchBox } from './components/search-box/search-box.component.jsx';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      monster: [],
+      searchField: '',
+      searchMonster: []
+    };
+
+    // this.handleChange = this.handleChange.bind(this);
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => {
+        return res.json();
+      })
+      .then(arr => {
+        this.setState({ monster: arr });
+        this.setState({ searchMonster: this.state.monster })
+      })
+  }
+
+  handleChange = (event) => {
+    this.setState({ searchField: event.target.value })
+  }
+  
+
+
+
+  render() {
+
+    const filteredMonster = this.state.monster.filter(obj => {
+      return obj.name.toLowerCase().includes(this.state.searchField.toLowerCase())
+    })
+
+ 
+    
+
+
+
+    return (
+
+
+
+      <div className="App">
+        <h1>MONSTERS ROLODEX</h1>
+
+        <SearchBox 
+          PLACEHOLDER='search monsters'
+          HANDLECHANGE={this.handleChange}
+        />
+
+        <CardList monsters={filteredMonster} />
+
+
+      </div>
+    )
+  }
 }
 
+
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -15,3 +15,6 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+//git remote add origin git@github.com:LAICHEEHOONG/monsters-rolodex.git
+//yarn add gh-pages
